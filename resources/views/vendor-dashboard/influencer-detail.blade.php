d3e106635779d17d204734fc717d1e4a1833feb3
bilalmuhammada <bilalahmada774@gmail.com>
2024-07-31 18:06:10 +0200

    Update influencer-detail.blade.php

--- resources/views/vendor-dashboard/influencer-detail.blade.php
+++ resources/views/vendor-dashboard/influencer-detail.blade.php
@@ -643,7 +643,11 @@
     {{-- @include("vendor-dashboard.influncerProfiledownload"); --}}
    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css">
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+    <!-- Magnific Popup JS -->
+ 
     <script>
         
     $(document).ready(function() {
