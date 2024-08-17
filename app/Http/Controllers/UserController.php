a784f14582dcafd1ca92e6cf3f6c826dc3ba29e0
bilalmuhammada <bilalahmada774@gmail.com>
2024-08-17 15:34:25 +0200

    p

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -268,7 +268,7 @@ class UserController extends Controller
         if ($image) {
             // dd($image);
              $image->delete();
-            return response()->json(['success' => true,'message'=>'Image Deleted']);
+            return response()->json(['success' => true,'message'=>'Deleted']);
         }
         return response()->json(['success' => false], 404);
     }
