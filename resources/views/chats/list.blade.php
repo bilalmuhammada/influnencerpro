98797585c1a2d6e2259415678a33f972218ff398
bilalmuhammada <bilalahmada774@gmail.com>
2024-08-18 22:09:19 +0200

    Update list.blade.php

--- resources/views/chats/list.blade.php
+++ resources/views/chats/list.blade.php
@@ -194,7 +194,7 @@ select::-ms-expand {
                                                     <div class="badge bgg-yellow badge-pill unread-count"
                                                          style="display: {{($login_user_id != $chat->latest_message_sender_id && $chat->unread_count > 0) ? 'block' : 'none'}} ">{{ $chat->unread_count }}</div>
                                                 </div>
-                                                <div style="display:flex; justify-content: flex-end; align-items: center;    margin-top: -10px;  margin-right: -82px;margin-bottom: 25px;">
+                                                <div style="display:flex; justify-content: flex-end; align-items: center;    margin-top: -10px;  margin-right: -63px;margin-bottom: 25px;">
                                                     <button class="btn btn-link favorite-chat" title="Favorite" style="padding: 0px;" data-chat-id="{{ $chat->id }}">
                                                         <i class="fa fa-heart" style="color: red;"></i>
                                                     </button>
