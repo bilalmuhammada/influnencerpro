a42a8c1f0d209a01c49b471dfb5f1d45a8a7c38d
bilalmuhammada <bilalahmada774@gmail.com>
2024-08-18 22:39:04 +0200

    Update list.blade.php

--- resources/views/chats/list.blade.php
+++ resources/views/chats/list.blade.php
@@ -146,7 +146,7 @@ select::-ms-expand {
                                 </div>
                                 <div class="col-md-2 edit">
                                     <div class="row">
-                                        <div class="col-md-12 text-center" style="margin: 9px 0px 0px 183px;">
+                                        <div class="col-md-12 text-center" style="margin: 9px 0px 0px 180px;">
                                             <i class="fa fa-pencil" id="edit-icon" style="color: rgb(9, 9, 166);"></i>
                                         </div>
                                     </div>
