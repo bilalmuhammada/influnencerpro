f83b688bf334976e6446365fd295fce6381673f1
bilalmuhammada <bilalahmada774@gmail.com>
2024-08-28 22:35:10 +0200

    Update about-us.blade.php

--- resources/views/modal/about-us.blade.php
+++ resources/views/modal/about-us.blade.php
@@ -1,13 +1,21 @@
 <!-- Modal -->
+
+<style>
+.modal-title{
+  margin-left: 172px;
+  color: blue;
+}
+</style>
+
 <div class="modal fade" id="aboutus" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
     <div class="modal-content">
       <div class="modal-header">
-        {{-- <h5 class="modal-title" id="exampleModalLabel">About us</h5> --}}
+        <h5 class="modal-title" id="exampleModalLabel">About us</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-       <h3 class="text-center" style="margin-top: -14px;">  About Us</h3>  
+       {{-- <h3 class="text-center" style="margin-top: -14px;">  About Us</h3>   --}}
       
        <p style="text-align: justify;    font-size: 18px;">
         InfluencerPro is the leading global visionary force at the intersection of Influencer Collaborations & Innovative Direct Connections between Influencers & Businesses. Our platform provides direct access to millions of Celebrity & Pro-Influencers, Media Artists & Talents across more than 30 business categories. Business Brands can easily Filter & Chat with Micro, Nano, Mega & Celebrity Influencers, ensuring high revenues and a guaranteed ROI on their marketing budgets.
