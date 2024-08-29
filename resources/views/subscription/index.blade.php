945eb059f49dc93e8628a861fa41ee19427026a9
bilalmuhammada <bilalahmada774@gmail.com>
2024-08-29 11:51:19 +0200

    push

--- resources/views/subscription/index.blade.php
+++ resources/views/subscription/index.blade.php
@@ -33,28 +33,33 @@
     <div class="col-md-4 mx-auto mt-1" >
         <form action="/session" method="POST">
             <input type="hidden" name="_token" value="{{csrf_token()}}">
-            <input type='hidden' name="total" value="199">
+            <input type='hidden' name="total" value="299">
     <div class="mx-auto" style="border:2px solid #A17A4E;padding:0px;padding-bottom:10px;background:;border-radius:5px;">
     <div  class="mx-auto text-center p-2">
        
     <span class="plan-h">Star</span>
     <div class="mx-auto" style="border:2px solid #A17A4E;width:40px;text-align:center;margin-top:-10px;"></div>
-    <h6 style="padding-top:10px;"><b>$ 199</b></h6>
+    <h6 style="padding-top:10px;"><b>$ 299</b></h6>
     <div class="mx-auto text-muted" style="text-align:center;margin-top:-10px;">1 month</div>
     </div>
     <hr>
     <div class="row mx-auto">
     <ul class="circle-check mx-auto" style="list-style-type:none;height:200px;padding-left:30px;">
-        <li><i class="fa fa-check-circle"></i>  Active: <b>30 days</b></li>
-        <li><i class="fa fa-check-circle"></i>  Chat Influencers: <b>150</b></li>
-        <li><i class="fa fa-check-circle"></i>  Favorite Influencers: <b>200</b></li>
-        <li><i class="fa fa-check-circle"></i>  View Influencers: <b>1 – 500K Followers</b></li>
-        <li><i class="fa fa-check-circle"></i>  View Influencers: <b> Based Country </b></li>
-        <li><i class="fa fa-check-circle"></i>  Access Socials: <b>Instagram, Facebook, Snapchat</b></li>
+        <li><i class="fa fa-check-circle"></i>  Active: 30 days</li>
+        <li><i class="fa fa-check-circle"></i>  View Categories: 30+</li>
+        <li><i class="fa fa-check-circle"></i>  View Profiles: Full Access</li>
+        <li><i class="fa fa-check-circle"></i>  Travel Calendars: Worldwide</li>
+        <li><i class="fa fa-check-circle"></i>  View Influencers & Talents: Globally</li>
+        <li><i class="fa fa-check-circle"></i>  Hire Influencers & Talents: Unlimited</li>
+        <li><i class="fa fa-check-circle"></i>  Influencers & Talents Collaboration: Unlimited</li>
+        <li><i class="fa fa-check-circle"></i>  View Influencers: Verified, 50M+ Followers, Celebrities</li>
+    
+        <li style="display:ruby-text;"><i class="fa fa-check-circle"></i>  Access Socials: <span style="display: inline-flex;">Instagram,Twitter, Facebook,Pinterest,<br>Snapchat, Tik Tok,You Tube,Website </span></li>
+</li>
         {{-- <li><i class="fa fa-times-circle text-danger"></i> Featured Days: <b>No</b></li> --}}
     </ul>
     </div>
-    <div class="mx-auto" style="text-align:center;margin-top: 90px;">
+    <div class="mx-auto" style="text-align:center;margin-top: 175px;">
         <input type='hidden' name="productname" value="Star Plan">
     <a href="">
         <button class="btn bt-plan" type="submit" id="checkout-live-button"><i class="fa fa-money"></i> SELECT PLAN</button>
@@ -69,14 +74,14 @@
     <div class="col-md-4 mx-auto mt-1" >
         <form action="/session" method="POST">
             <input type="hidden" name="_token" value="{{csrf_token()}}">
-            <input type='hidden' name="total" value="599">
+            <input type='hidden' name="total" value="759">
     <div class="mx-auto" style="border:2px solid #A17A4E;padding:0px;padding-bottom:10px;background:;border-radius:5px;">
     <div  class="mx-auto text-center p-2">
         <div style="text-align:center;">
         <span class="plan-h">Gold</span>
         </div>
     <div class="mx-auto" style="border:2px solid #A17A4E;width:40px;text-align:center;margin-top:-10px;"></div>
-    <h6 style="padding-top:10px;"><b>$ 599</b>
+    <h6 style="padding-top:10px;"><b>$ 759</b>
          {{-- <b>(Save 15%)</b> --}}
         </h6>
     <div class="mx-auto text-muted" style="text-align:center;margin-top:-10px;">3 Months</div>
@@ -84,16 +89,19 @@
     <hr>
     <div class="row mx-auto">
     <ul class="circle-check" style="list-style-type:none;height:200px;padding-left:30px;">
-        <li><i class="fa fa-check-circle"></i>  Active: <b>90 days</b></li>
-        <li><i class="fa fa-check-circle"></i>  Chat Influencers: <b>500</b></li>
-        <li><i class="fa fa-check-circle"></i>  Favorite Influencers: <b>700</b></li>
-        <li><i class="fa fa-check-circle"></i>  View Influencers: <b>1 – 1M Followers</b></li>
-        <li><i class="fa fa-check-circle"></i>  View Influencers: <b>  Based Country  </b></li>
-        <li><i class="fa fa-check-circle"></i>  View Influencers: <b>  Worldwide </b></li>
-        <li><i class="fa fa-check-circle"></i>  Access Socials: <b style="display: inline-flex;">Instagram, Facebook, Snapchat,<br> Tik Tok</b></li>
+        <li><i class="fa fa-check-circle"></i>  Active: 90 days</li>
+        <li><i class="fa fa-check-circle"></i>  View Categories: 30+</li>
+        <li><i class="fa fa-check-circle"></i>  View Profiles: Full Access</li>
+        <li><i class="fa fa-check-circle"></i>  Travel Calendars: Worldwide</li>
+        <li><i class="fa fa-check-circle"></i>  View Influencers & Talents: Globally</li>
+        <li><i class="fa fa-check-circle"></i>  Hire Influencers & Talents: Unlimited</li>
+        <li><i class="fa fa-check-circle"></i>  Influencer & Talent Collaborations: Unlimited</li>
+        <li><i class="fa fa-check-circle"></i>  View Influencers: Verified, 50M+ Followers, Celebrities</li>
+        <li style="display:ruby-text;"><i class="fa fa-check-circle"></i>  Access Socials: <span style="display: inline-flex;">Instagram,Twitter, Facebook,Pinterest,<br>Snapchat, Tik Tok,You Tube,Website </span></li>
+</li>
     </ul>
     </div>
-    <div class="mx-auto" style="text-align:center;margin-top: 90px;">
+    <div class="mx-auto" style="text-align:center;margin-top: 175px;">
         <input type='hidden' name="productname" value="Gold Plan">
         <a href="">
             <button class="btn bt-plan" type="submit" id="checkout-live-button"><i class="fa fa-money"></i> SELECT PLAN</button>
@@ -108,13 +116,13 @@
     <div class="col-md-4 mx-auto mt-1" >
         <form action="/session" method="POST">
             <input type="hidden" name="_token" value="{{csrf_token()}}">
-            <input type='hidden' name="total" value="1299">
+            <input type='hidden' name="total" value="1439">
     <div class="mx-auto" style="border:2px solid #A17A4E;padding:0px;padding-bottom:10px;background:;border-radius:5px;">
     <div  class="mx-auto text-center p-2">
     <span class="plan-h">Pro</span>
     <div class="mx-auto" style="border:2px solid #A17A4E;width:40px;text-align:center;margin-top:-10px;"></div>
     <h6 style="padding-top:10px;">
-        <b>$ 1,299</b> 
+        <b>$ 1,439</b> 
         {{-- <b>(Save 20%)</b>  --}}
     </h6>
     <div class="mx-auto text-muted" style="text-align:center;margin-top:-10px;">6 Months</div>
@@ -122,19 +130,21 @@
     <hr>
     <div class="row mx-auto">
     <ul class="circle-check" style="list-style-type:none;height:200px;padding-left:30px;">
-        <li><i class="fa fa-check-circle"></i>  Active: <b>180 Days</b></li>
-        <li><i class="fa fa-check-circle"></i>  Chat Influencers: <b>Unlimited</b></li>
-        <li><i class="fa fa-check-circle"></i>  Favorite Influencers: <b>Unlimited</b></li>
-        <li><i class="fa fa-check-circle"></i>  View Influencers: <b>Unlimited Followers</b></li>
-        <li><i class="fa fa-check-circle"></i>  View Influencers: <b>  Based Country  </b></li>
-        <li><i class="fa fa-check-circle"></i>  Celebrity Influencers: <b>  Worldwide </b> <li>
-        <li><i class="fa fa-check-circle"></i>  View Influencers: <b>  Worldwide </b></li>
-       
-        <li><i class="fa fa-check-circle"></i>  Access Socials: <b style="display: inline-flex;">Instagram, Facebook, Snapchat,<br> Tik Tok,You Tube, Website</b></li>
+        <li><i class="fa fa-check-circle"></i>  Active: 180 Days</li>
+        <li><i class="fa fa-check-circle"></i>  View Categories: 30+</li>
+        <li><i class="fa fa-check-circle"></i>  View Profiles: Full Access</li>
+        <li><i class="fa fa-check-circle"></i>  Travel Calendars: Worldwide</li>
+        <li><i class="fa fa-check-circle"></i>  View Influencers & Talents: Globally</li>
+        <li><i class="fa fa-check-circle"></i>  Hire Influencers & Talents: Unlimited <li>
+        <li><i class="fa fa-check-circle"></i>  Influencer & Talent Collaborations: Unlimited</li>
+        <li><i class="fa fa-check-circle"></i>  View Influencers: Verified, 50M+ Followers, Celebrities</li>
+        <li><i class="fa fa-check-circle"></i>  Influencer & Talent Collaborations: Unlimited</li>
+        <li><i class="fa fa-check-circle"></i>  Influencer & Talent Collaborations: Unlimited</li>
+        <li style="display:ruby-text;"><i class="fa fa-check-circle"></i>  Access Socials: <span style="display: inline-flex;">Instagram,Twitter, Facebook,Pinterest,<br>Snapchat, Tik Tok,You Tube,Website </span></li>
 
     </ul>
     </div>
-    <div class="mx-auto" style="text-align:center;margin-top: 90px;">
+    <div class="mx-auto" style="text-align:center;margin-top: 175px;">
         <input type='hidden' name="productname" value="Pro Plan">
         <a href="">
             <button class="btn bt-plan" type="submit" id="checkout-live-button"><i class="fa fa-money"></i> SELECT PLAN</button>
