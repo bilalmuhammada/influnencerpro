7db799de30844b768e09fae702acac343be5625a
bilalmuhammada <bilalahmada774@gmail.com>
2024-09-04 15:19:43 +0200

    Update topbar.blade.php

--- resources/views/layout/web/topbar.blade.php
+++ resources/views/layout/web/topbar.blade.php
@@ -262,12 +262,12 @@
         console.log('Flag URL:', flagUrl);
 
         if (flagUrl) {
-        return $('<span style="font-size:14px;font-weight:bold;  white-space: nowrap;  padding:8px;"><img src="' + flagUrl + '" class="img-flag" style="width: 20px; height:14px; margin-right: 5px;" /> ' + option.text + '</span>');
+        return $('<span style="font-size:14px;font-weight:bold;  white-space: nowrap;  padding:8px;"><img src="' + flagUrl + '" class="img-flag" style="width: 20px;margin-bottom: 4px; height:14px; margin-right: 5px;" /> ' + option.text + '</span>');
     } else {
         // return $('<span style="font-size:18px;margin-left:25px;font-weight:600; "><img src="'   '" class="img-flag" style="width: 20px; height:14px; margin-right: 3px;" /> ' + option.text + '</span>');
     
         return $('<span style="font-size:14px;font-weight:bold;    white-space: nowrap; padding:8px;">' +
-    '<img src="{{ asset("/assets/img/social-icon/lang.png") }}" class="img-flag" style="width: 20px; height:18px; margin-bottom: 4px; margin-right: 5px;" /> ' +
+    '<img src="{{ asset("/assets/img/social-icon/lang.png") }}" class="img-flag" style="width: 20px; height:18px;  margin-right: 5px;margin-bottom: 4px;" /> ' +
     option.text + 
     '</span>');}
         // return $('<span style="font-size:18px;"><img src="' + flagUrl + '" class="img-flag" style="width: 30px; height: 20px; margin-right: 0px;" /> ' + option.text + '</span>');
