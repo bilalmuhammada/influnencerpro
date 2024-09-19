eebae694427d529b84c428868f2d6c69e226dc2e
bilalmuhammada <bilalahmada774@gmail.com>
2024-09-19 10:33:57 +0200

    Update home.blade.php

--- resources/views/vendor-dashboard/home.blade.php
+++ resources/views/vendor-dashboard/home.blade.php
@@ -122,7 +122,7 @@ width: 200px !important;
     min-height: 40px !important;
 }
 #select2-language_dropdown-container{
-    margin-left: 5px !important;
+    margin-left: 6px !important;
 }
 #select2-nationality_id-results{
 padding-left: 12px !important;
