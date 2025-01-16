805d927dcd9a2ffa6b5c2e1fe6d5b8b02d1800d9
bilalmuhammada <bilalahmada774@gmail.com>
2025-01-16 20:38:28 +0100

    Update ChatController.php

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -27,7 +27,7 @@ class ChatController extends Controller
         $chats = Chat::with(['messages'])
             ->where('first_user_id', SiteHelper::getLoginUserId())
             ->orWhere('second_user_id', SiteHelper::getLoginUserId());
-        dd($chats->get(), session()->get('role'));
+        // dd($chats->get(), session()->get('role'));
         if ($request->i) {
                 $chats = $chats->where('second_user_id', '=', $request->i);
         } 
