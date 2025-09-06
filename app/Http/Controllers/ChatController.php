061d751e2233dea5272423657deaa16255480d5d
bilalmuhammada <bilalahmada774@gmail.com>
2025-09-06 19:27:51 +0200

    Update ChatController.php

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -55,7 +55,7 @@ class ChatController extends Controller
                 }
         
                 // If you want the date groups in order (latest day last):
-                ksort($groupedMessages); // ascending by date
+                krsort($groupedMessages); // ascending by date
                 // or for newest day first:
                 // krsort($groupedMessages);
         
